fba428f23b6f7c824b6a6196c40dc039c0abda32
Clapboiz <phamconglap03@gmail.com>
2024-01-24 00:55:44 +0700

    homepage server

--- Server/src/routes/web.js
+++ Server/src/routes/web.js
@@ -1,13 +1,17 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("Welcome to backend!");    
-    });
+  // router.get('/', (req, res) => {
+  //     return res.send("Welcome to backend!");
+  // });
 
-    return app.use("/", router);
-}
+  router.get("/", homeController.getHomePage);
+  router.get("/about", homeController.getAboutPage);
+
+  return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
